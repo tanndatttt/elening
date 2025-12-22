7728d6f75c496995db95f41842f8d2238f3c847a
tanndatttt <tandatt110727@gmail.com>
2025-12-22 15:50:29 +0700

    elerning 1

---
+++ el1.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "Nhap 3 so: ";
+	cin >> a >> b >> c;
+	// tạo mảng
+	int day_so[3] = { a, b, c };
+	int n = 3;
+	// dùng thuật toán sắp xếp chọn (selection sort)
+	for (int i = 0; i < n - 1; i += 1)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (day_so[j] < day_so[min])
+			{
+				min = j;
+			}
+		}
+		swap(day_so[i], day_so[min]);
+	}
+	// day_so [n] n là thứ tự để in ra màn hình hoặc teminal
+	cout << "so o giua la: " << day_so[1];
+}
