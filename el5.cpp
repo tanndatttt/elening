b9e4176aa0b8437a53c825eb6e61e2bde2f51a60
tanndatttt <tandatt110727@gmail.com>
2025-12-22 16:28:27 +0700

    elering 5

---
+++ el5.cpp
@@ -0,0 +1,113 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Nhap bang cuu chuong: ";
+	int n;
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+		cout << " 1*1=1" << endl;
+		cout << " 1*2=2" << endl;
+		cout << " 1*3=3" << endl;
+		cout << " 1*4=4" << endl;
+		cout << " 1*5=5" << endl;
+		cout << " 1*6=6" << endl;
+		cout << " 1*7=7" << endl;
+		cout << " 1*8=8" << endl;
+		cout << " 1*9=9" << endl;
+		break;
+	case 2:
+		cout << " 2*1=2" << endl;
+		cout << " 2*2=4" << endl;
+		cout << " 2*3=6" << endl;
+		cout << " 2*4=8" << endl;
+		cout << " 2*5=10" << endl;
+		cout << " 2*6=12" << endl;
+		cout << " 2*7=14" << endl;
+		cout << " 2*8=16" << endl;
+		cout << " 2*9=18" << endl;
+		break;
+	case 3:
+		cout << " 3*1=3" << endl;
+		cout << " 3*2=6" << endl;
+		cout << " 3*3=9" << endl;
+		cout << " 3*4=12" << endl;
+		cout << " 3*5=15" << endl;
+		cout << " 3*6=18" << endl;
+		cout << " 3*7=21" << endl;
+		cout << " 3*8=24" << endl;
+		cout << " 3*9=27" << endl;
+		break;
+	case 4:
+		cout << " 4*1=4" << endl;
+		cout << " 4*2=8" << endl;
+		cout << " 4*3=12" << endl;
+		cout << " 4*4=16" << endl;
+		cout << " 4*5=20" << endl;
+		cout << " 4*6=24" << endl;
+		cout << " 4*7=28" << endl;
+		cout << " 4*8=32" << endl;
+		cout << " 4*9=36" << endl;
+		break;
+	case 5:
+		cout << " 5*1=5" << endl;
+		cout << " 5*2=10" << endl;
+		cout << " 5*3=15" << endl;
+		cout << " 5*4=20" << endl;
+		cout << " 5*5=25" << endl;
+		cout << " 5*6=30" << endl;
+		cout << " 5*7=35" << endl;
+		cout << " 5*8=40" << endl;
+		cout << " 5*9=45" << endl;
+		break;
+	case 6:
+		cout << " 6*1=6" << endl;
+		cout << " 6*2=12" << endl;
+		cout << " 6*3=18" << endl;
+		cout << " 6*4=24" << endl;
+		cout << " 6*5=30" << endl;
+		cout << " 6*6=36" << endl;
+		cout << " 6*7=42" << endl;
+		cout << " 6*8=48" << endl;
+		cout << " 6*9=54" << endl;
+		break;
+	case 7:
+		cout << " 7*1=7" << endl;
+		cout << " 7*2=14" << endl;
+		cout << " 7*3=21" << endl;
+		cout << " 7*4=28" << endl;
+		cout << " 7*5=35" << endl;
+		cout << " 7*6=42" << endl;
+		cout << " 7*7=49" << endl;
+		cout << " 7*8=56" << endl;
+		cout << " 7*9=63" << endl;
+		break;
+	case 8:
+		cout << " 8*1=8" << endl;
+		cout << " 8*2=16" << endl;
+		cout << " 8*3=24" << endl;
+		cout << " 8*4=32" << endl;
+		cout << " 8*5=40" << endl;
+		cout << " 8*6=48" << endl;
+		cout << " 8*7=56" << endl;
+		cout << " 8*8=64" << endl;
+		cout << " 8*9=72" << endl;
+		break;
+	case 9:
+		cout << " 9*1=9" << endl;
+		cout << " 9*2=18" << endl;
+		cout << " 9*3=27" << endl;
+		cout << " 9*4=36" << endl;
+		cout << " 9*5=45" << endl;
+		cout << " 9*6=54" << endl;
+		cout << " 9*7=63" << endl;
+		cout << " 9*8=72" << endl;
+		cout << " 9*9=81" << endl;
+		break;
+	default: 
+		cout << "Khong co bang cuu chuong nay!" << endl;
+		break;
+	}
+}
