77bcb9ff3caacaa467ffa59abc949454a87354c6
tanndatttt <tandatt110727@gmail.com>
2025-12-22 15:51:28 +0700

    elerning 2

---
+++ el2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a, b, c, n;
+	cout << "nhap 3 so a,b,c: ";
+	cin >> a >> b >> c;
+	cout << "nhap n: ";
+	cin >> n;
+	a1 = n - a;
+	b1 = n - b;
+	c1 = n - c;
+	// xet xem so nao gan n nhat
+	if (a1 < b1 && a1 < c1)
+		cout << "so gan n nhat la: " << a;
+	else if (b1 < a1 && b1 < c1)
+		cout << "so gan n nhat la: " << b;
+	else
+		cout << "so gan n nhat la: " << c;
+}
